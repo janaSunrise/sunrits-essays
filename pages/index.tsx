31f19b30fc0b6dde024bf0ce5a73d1fe322b3b91
Sunrit Jana <warriordefenderz@gmail.com>
2023-05-03 23:01:48 +0530

    new essay + site updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ interface Props {
 const Home = ({ essays }: Props) => {
   return (
     <div className="max-w-5xl px-8 py-4 mx-auto">
-      <h2 className="text-3xl py-4 font-semibold">essays crafted with care</h2>
+      <h2 className="text-3xl pb-4 font-semibold">essays crafted with care</h2>
 
       {essays.map(essay => (
         <div key={essay.slug} className="py-2">
@@ -30,7 +30,13 @@ const Home = ({ essays }: Props) => {
 };
 
 export const getStaticProps: GetStaticProps<Props> = () => {
-  return { props: { essays: allEssays } };
+  const allSortedEssays = allEssays.sort((a, b) => {
+    if (new Date(a.date) > new Date(b.date)) return -1;
+    if (new Date(a.date) < new Date(b.date)) return 1;
+    return 0;
+  });
+
+  return { props: { essays: allSortedEssays } };
 };
 
 export default Home;
